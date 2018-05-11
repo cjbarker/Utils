044364b9ef2362a33cb8c176756fb5c1cd9ac498
CJ Barker <cjbarker@gmail.com>
2018-05-11 12:13:35 -0700

    fix exit and split config/creds

--- linux_scripts/scripts/aws_set_creds.sh
+++ linux_scripts/scripts/aws_set_creds.sh
@@ -10,26 +10,27 @@
 # #########################################################
 
 CONFIG_FILE=~/.aws/config
+CRED_FILE=~/.aws/credentials
 
-if [ ! -f ${CONFIG_FILE} ]; then
-    exit 1
+if [ ! -f ${CONFIG_FILE} -a ! -f ${CRED_FILE} ]; then
+    return
 fi
 
 if [ -z ${AWS_PROFILE} ]; then
-    exit 2
+    return
 fi
 
 PROFILE="profile ${AWS_PROFILE}"
-ACCESS_KEY_ID=`cat ${CONFIG_FILE} | grep "${PROFILE}" -A 3 | grep "aws_access_key_id" | cut -d'=' -f2 | tr -d '[:space:]'`
-SECRET_ACCESS_KEY=`cat ${CONFIG_FILE} | grep "${PROFILE}" -A 3 | grep "aws_secret_access_key" | cut -d'=' -f2 | tr -d '[:space:]'`
 REGION=`cat ${CONFIG_FILE} | grep "${PROFILE}" -A 3 | grep "region" | cut -d'=' -f2 | tr -d '[:space:]'` 
 
+PROFILE="${AWS_PROFILE}"
+ACCESS_KEY_ID=`cat ${CRED_FILE} | grep "${PROFILE}" -A 3 | grep "aws_access_key_id" | cut -d'=' -f2 | tr -d '[:space:]'`
+SECRET_ACCESS_KEY=`cat ${CRED_FILE} | grep "${PROFILE}" -A 3 | grep "aws_secret_access_key" | cut -d'=' -f2 | tr -d '[:space:]'`
+
 if [ -z ${ACCESS_KEY_ID} -a -z ${SECRET_ACCESS_KEY} ]; then
-    exit 3
+    return
 fi
 
 export AWS_ACCESS_KEY_ID="${ACCESS_KEY_ID}"
 export AWS_SECRET_ACCESS_KEY="${SECRET_ACCESS_KEY}"
 export AWS_DEFAULT_REGION="${REGION}"
-
-exit 0
