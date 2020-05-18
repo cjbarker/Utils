aa702de29f4d212ab69cf69f21577fab3ea8cdb1
CJ Barker <cjbarker@gmail.com>
2020-05-18 11:07:59 -0700

    Initial placeholder for powershell dotFile for Windoze

---
+++ dotFiles/install-windows.ps1
@@ -0,0 +1,12 @@
+# ######################################################
+# Windows Powershell dotFile for setting up environment
+#
+# For Mac & *nix environments running PS see:
+# https://github.com/PowerShell/PowerShell
+#
+# Usage: pwsh install-windows.ps1
+#
+# ######################################################
+
+Write-Host '****** Windoze dotFile *******'
+
