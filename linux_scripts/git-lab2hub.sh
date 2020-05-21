5fffe8843dc9b46fb9b0a97270e6dda53ce6c7c5
cjbarker <cjbarker@gmail.com>
2020-05-21 12:36:50 -0700

    Find GitLab project and enable mirror to GitHub

--- linux_scripts/git-lab2hub.sh
+++ linux_scripts/git-lab2hub.sh
@@ -5,8 +5,9 @@
 #
 # Handles creating remote repo, settings up mirroring & activation.
 #
-# Usage: ./git-lab2hub.sh [access-token-file-name]
-#        access-token-file-name: access token for GitHub
+# Usage: ./git-lab2hub.sh [access-gh_token-file-name] [access-gl-token-file-name]
+#        access-gh_token-file-name: access token for GitHub
+#        access-gl_token-file-name: access token for GitLab
 #
 #  echo $?
 #
@@ -21,15 +22,17 @@ CMDS[0]='git'
 CMDS[1]='cat'
 CMDS[2]='touch'
 CMDS[3]='sed'
+CMDS[4]='jq'
 
 function echoerr {
     echo "$@" 1>&2
 }
 
 function usage() {
-    echo -e "Usage: $0 [repo-name] [access-token-file-name]"
+    echo -e "Usage: $0 [repo-name] [access-gh_token-file-name] [access-gl token-file-name]"
     echo -e "\trepo-name: name of repository to mirror"
-    echo -e "\taccess-token-file-name: access token for GitHub"
+    echo -e "\taccess-gh_token-file-name: access token for GitHub"
+    echo -e "\taccess-gl_token-file-name: access token for GitLab"
     exit 1
 }
 
@@ -61,7 +64,7 @@ done
 
 # call usage() function if filename not supplied
 [[ $# -eq 0 ]] && usage
-[[ $# -ne 2 ]] && usage
+[[ $# -ne 3 ]] && usage
 
 if [ -z "${1}" ]; then
     echoerr "Invalid repo name - cannot be null."
@@ -69,17 +72,23 @@ if [ -z "${1}" ]; then
 fi
 
 if [ ! -f "${2}" ]; then
-    echoerr "Invalid file for access token: ${2}"
+    echoerr "Invalid file for GitHub access token: ${2}"
     exit 3
+fi
+if [ ! -f "${3}" ]; then
+    echoerr "Invalid file for GitLab access token: ${3}"
+    exit 4
 fi
 
 repo=`echo ${1} | sed "s/.git//g"`
-token=`cat ${2}`
+gh_token=`cat ${2}`
+gl_token=`cat ${3}`
 username="cjbarker" # TODO take cmdline arg
-url="https://${username}:${token}@github.com/${username}/${repo}.git"
+url="https://${username}:${gh_token}@github.com/${username}/${repo}.git"
 
 #echo ${repo}
-#echo ${token}
+#echo ${gh_token}
+#echo ${gl_token}
 #echo ${url}
 
 if [ ! -d "${repo}" ]; then
@@ -87,7 +96,7 @@ if [ ! -d "${repo}" ]; then
 fi
 
 # create GH repo via API
-curl -u "${username}:${token}" https://api.github.com/user/repos -d "{\"name\":\"${repo}\"}"
+curl -u "${username}:${gh_token}" https://api.github.com/user/repos -d "{\"name\":\"${repo}\"}"
 
 # create actual repo
 cd ${repo}
@@ -98,6 +107,17 @@ git commit -m "Initial creation via ${0}"
 git remote add origin "${url}"
 git push -u origin master
 
-# configure GL repo for push mirror to GH
+# get GitLab Project ID
+projectId=`curl -s https://gitlab.com/api/v4/users/${username}/projects | jq -r '.[] | .id, .name' | grep -B 1 "${repo}" | grep -v "${repo}"`
+
+if [ -z "${projectId}" ]; then
+    echoerr "Unable to find GitLab project id for repo ${repo}"
+    exit 5
+fi
+
+#echo "Project ID: ${projectId}"
+
+# Create a Remote Mirror in GitLab Project to GitHub
+curl --request POST --data "url=https://${username}:${gh_token}@github.com/${username}/${repo}.git&enabled=true" --header "PRIVATE-TOKEN:${gl_token}" "https://gitlab.com/api/v4/projects/${projectId}/remote_mirrors"
 
 exit 0
