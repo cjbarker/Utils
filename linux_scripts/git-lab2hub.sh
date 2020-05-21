4c7a87439a4ed12f98e7fda28bc490f963ce62b8
CJ Barker <cjbarker@gmail.com>
2020-05-21 10:31:49 -0700

    Repo and token checks

--- linux_scripts/git-lab2hub.sh
+++ linux_scripts/git-lab2hub.sh
@@ -15,12 +15,17 @@
 #
 # ###############################################################
 
+# Required built-ins
+declare -a CMDS
+CMDS[0]='git'
+
 function echoerr {
     echo "$@" 1>&2
 }
 
 function usage() {
-    echo -e "Usage: $0 [access-token-file-name]"
+    echo -e "Usage: $0 [repo-name] [access-token-file-name]"
+    echo -e "\trepo-name: name of repository to mirror"
     echo -e "\taccess-token-file-name: access token for GitHub"
     exit 1
 }
@@ -41,6 +46,34 @@ function cmd_exists {
 # M A I N     L O G I C
 # *************************************************************
 
+# validate binaries
+for cmd in "${CMDS[@]}"
+do
+    #echo ${cmd}
+    rc=`cmd_exists ${cmd}`
+    if [ "$rc" -ne "0" ]; then
+        exit $rc
+    fi
+done
+
 # call usage() function if filename not supplied
 [[ $# -eq 0 ]] && usage
-[[ $# -ne 1 ]] && usage
+[[ $# -ne 2 ]] && usage
+
+if [ -z "${1}" ]; then
+    echoerr "Invalid repo name - cannot be null."
+    exit 2
+fi
+
+if [ ! -f "${2}" ]; then
+    echoerr "Invalid file for access token: ${2}"
+    exit 3
+fi
+
+repo=`cat ${1} | sed "s/.git//g"`
+token=`cat ${2}`
+
+# create GH repo
+#echo ${repo}
+#echo ${token}
+
